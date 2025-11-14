c72450246115983e88fcf88c1de14a5f24536e4d
MeidiLprog <m.lef3105@gmail.com>
2025-11-15 00:35:38 +0100

    Logistic regression + plot of loss + GD

--- LR_implementation.py
+++ LR_implementation.py
@@ -26,13 +26,68 @@ class LogisticRegression:
     def Loss(self, X,y,beta):
         p = sigmoid(X @ beta)
         #I added an epsilone to prevent log(0) otherwise it's gonna be infinite and the program shall crash
-        eps = 1e-12
+        eps = 1e-9
         cost = -(1/len(y))* np.sum(y*np.log(p+eps) + (1-y)*np.log(1-p+eps))
         return cost
 
-    def fit(X,y):
-        pass
+    def fit(self,X,y):
+        
 
-    def predict(X):
-        pass
+        #I need to add the biais to X beta0 + beta1 x1 + beta2 x2 ... beta n xn
+        X = np.hstack([np.ones((X.shape[0],1)),X])
 
+        #Retrieving Highst lambda value from SVD
+        _, S, _ = np.linalg.svd(X,full_matrices=False)
+        _highest_lambda = S[0]
+
+        #here I initialize beta as I need to do a matrix multiplicate with X
+        beta = np.zeros(X.shape[1])
+
+
+        alpha = 4/(_highest_lambda**2)
+
+        for i in range(self.iterations):
+           
+           #calculation of the gradient here sigmoid play the role of X*THETA in MSE, we just use the probalistic approach
+            gradient = X.T @ (sigmoid(X @ beta) - y) 
+            #calculation of parameters beta without momentum, applying a normal GD       
+            beta = beta - alpha * gradient
+           
+            self.Loss_cost.append(self.Loss(X,y,beta)) # I save the beta to plot the convergence of it
+            if np.linalg.norm(gradient) < 1e-6:
+                print("Local globalizer of f found \n")
+                break
+        self.beta = beta
+
+    def predict(self,X):
+        
+        X = np.hstack([np.ones((X.shape[0],1)),X])
+
+        z = X @ self.beta
+
+        p = sigmoid(z)
+        return (p >= 0.5).astype(int)
+
+import matplotlib.pyplot as plt
+
+
+X , y = datasets[0]
+
+model = LogisticRegression(optimizer="gradient", iterations=200)
+model.fit(X,y)
+
+
+y_pred = model.predict(X)
+
+
+plt.scatter(X[:,0], X[:,1],c=y_pred,cmap="bwr",alpha=0.7)
+plt.title("Classification using logistic regression")
+plt.show()
+
+#display loss function
+
+plt.plot(model.Loss_cost)
+plt.title("Evolution of the loss function")
+plt.xlabel("Iterations")
+plt.ylabel("Loss")
+plt.show()
