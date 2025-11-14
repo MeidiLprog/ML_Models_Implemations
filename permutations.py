6e72053d25f986700cfbc91edc3824bb90270616
MeidiLprog <m.lef3105@gmail.com>
2025-11-14 16:04:30 +0100

    backtracking problem 1 done

--- permutations.py
+++ permutations.py
@@ -5,19 +5,60 @@ import random
 '''
 Backtracking: Mind approach ? a 
 
+Choices: All numbers in nums
+Constraint: can't reuse the same number more than once in our path
+Base Case: When the path length equals len(nums) which makes the first branch completed
+Backtrack step : I do pop the last number added
+Finally: I use backtracking([]) to start construction my list
 
+Backtracking Template:
+def backtrack(params):
+    if base_case_condition:
+        save_result
+        return
+
+    for choice in choices:
+        if violates_constraints:
+            continue
+
+        make_choice
+        backtrack(updated_params)
+        undo_choice  # Backtracking Step
 '''
 
 
 class Permut:
-    result = []
-    def __init__(self,perms):
-        pass
+    
+    def permute(self,nums):
+        result = []
 
-    def backtracking(self):
-        pass
+        def backtracking(path):
+        
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
+
+            for n in nums:
+                if n in path: continue
+                path.append(n)
+                backtracking(path)
+                path.pop()
+        backtracking([])
+        return result
 
 
+'''
+Permutation problems
 
+'''
+
+# Test
+perms = Permut()
+
+nums = [1,2,3]
+
+print("Backtracking done\n")
+print()
+print(f"Done : {perms.permute(nums)}")
 
 
