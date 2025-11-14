82ccd0f6cec58710c9c68cd531af56abd0610ce6
MeidiLprog <m.lef3105@gmail.com>
2025-11-14 17:35:09 +0100

    Sudoky + plot

--- sudoku.py
+++ sudoku.py
@@ -39,11 +39,12 @@ def is_valid(p,l,c,n):
 
     return True
 
-def sudoku(p):
+def sudoku(puzzle):
+   
      
     def backtracking():
         #base_case
-        empty = find_empty_cell(p)
+        empty = find_empty_cell(puzzle)
         if empty is None:
             return True
         l, c = empty 
@@ -51,20 +52,20 @@ def sudoku(p):
         #constraint
         for n in range(1, 10):
 
-            if not is_valid(p, l, c, n):
+            if not is_valid(puzzle, l, c, n):
                 continue
             #make choice
-            p[l][c] = n
+            puzzle[l][c] = n
             #backtracking
             if backtracking():
                 return True
             #unmake choice
-            p[l][c] = 0   
+            puzzle[l][c] = 0   
 
         return False  
 
     backtracking()
-    return p
+    return puzzle
 
 
 
@@ -84,13 +85,36 @@ grid = np.array([
 ])
 
 
-solved = sudoku(grid)
+solved = sudoku(grid.copy())
 print(solved)
 
+def showSudoku(puzzle,puzzled_solved):
+    
+    plt.figure(figsize=(6,6)) #window size
+    ax = plt.gca() #get current axe
 
+    ax.imshow(np.ones((9,9)),cmap="gray_r") #display the matrix as an image and the gray_r just means reversed so white -> black
 
+    ax.set_xticks([])
+    ax.set_yticks([])
 
+    for i in range(10):
+        lw = 3 if i%3 == 0 else 1
+        ax.axhline(i - 0.5, color = 'black', linewidth=lw)
+        ax.axvline(i - 0.5, color = 'black', linewidth=lw)
 
+    
+    for l in range(9):
+        for c in range(9):
+            if puzzle[l][c] != 0:
+                color = 'black'
+            else:
+                color = 'blue'
+            ax.text(c,l,puzzled_solved[l][c],
+                    ha = 'center', va='center',
+                    fontsize=12,color=color
+                    
+                    )
 
-
-
+    plt.show()
+showSudoku(grid,solved)
