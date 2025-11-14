b015cea901a82689bc83a450bd32c7631cd9f3f5
MeidiLprog <m.lef3105@gmail.com>
2025-11-14 19:23:49 +0100

    Setting evertyhing up

--- LR_implementation.py
+++ LR_implementation.py
@@ -1,2 +1,29 @@
 #here's the the implementation of the logistic regression
 
+import numpy as np
+import pandas as pd
+import cvxpy as cp
+from sklearn.datasets import make_moons,make_circles,make_blobs
+
+datasets = [
+    make_moons(noise = 0.3, random_state = 0),
+    make_circles(noise=0.2, factor=0.5, random_state=1),
+    make_blobs(n_samples=100, centers=2, n_features=2, center_box=(0, 20), random_state=0)
+]
+
+def sigmoid(z : int):
+    func = 1/(1+ np.exp(-z))
+    return func
+
+class LogisticRegression:
+    def __init__(self, optimizer="gradient", iterations=200):
+        self.optimize = optimizer
+        self.iterations = iterations
+        
+
+    def fit(X,y):
+        pass
+
+    def predict(X):
+        pass
+
