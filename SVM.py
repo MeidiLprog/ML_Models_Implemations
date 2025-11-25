69591255f6e88fe8a59d5e217a589d168c1fba21
MeidiLprog <m.lef3105@gmail.com>
2025-11-26 00:17:55 +0100

    Building Kernel Calculations + Objective function to retrieve the best alpha for the hyperplan all of this using CVXPY

--- SVM.py
+++ SVM.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+
+
+
 class SVM:
     def __init__(self,kernel,C):
         if not isinstance(kernel,str):
@@ -20,8 +23,39 @@ class SVM:
         if kernel == "linear":
             self.C = 0.025
 
+        self.alpha = None
+        self.X = None
+        self.y = None
+        self.K_function = None
+
+    def kernel_calculation(self,X : np.matrix,ker_nel = 0):
+        #we are to pick what kind of kernel we use
+        if ker_nel == 0: 
+            for i in range(len(X)):
+                for j in range(len(X)):
+                    self.K_function = X[i] @ X[j]
+        else:
+            for i in range(len(X)):
+                for j in range(len(X)):
+                    
+
+        return
+
     def fit(self,X,y):
-        pass
+        if len(X) == 0 or len(y) == 0:
+            raise ValueError("Matrices aren't to be empty")
+        self.X = X
+        self.y = y 
+        obs,variables = X.Shape
+        matrix = self.zeros(obs,obs)
+        self.kernel_calculation(matrix,ker_nel=1)
+        print(f"{obs} available\n")
+        print(f"{variables} available\n")
+        
+
+
+        return
+
 
     def predict(self,X):
         pass
